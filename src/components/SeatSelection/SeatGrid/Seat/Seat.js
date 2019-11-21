5197a562bcd3cd9cbebaa753d37557e04e41c2c7
Oscar Ko <OscarKoEmail@gmail.com>
2019-11-21 14:03:11 -0500

    Create seat grid and seats

--- src/components/SeatSelection/SeatGrid/Seat/Seat.js
+++ src/components/SeatSelection/SeatGrid/Seat/Seat.js
@@ -1,10 +1,11 @@
 import React from "react";
+import classes from "./Seat.module.css"
 
 const seat = (props) => {
 
     return (
-        <div>
-            seat
+        <div className={classes.Seat}>
+            {props.children}
         </div>
     )
 }
--- src/components/SeatSelection/SeatGrid/SeatGrid.js
+++ src/components/SeatSelection/SeatGrid/SeatGrid.js
@@ -1,12 +1,29 @@
 import React from "react";
 import Seat from "./Seat/Seat";
+import classes from "./SeatGrid.module.css"
+
+let rows = ["A", "B", "C", "D"];
+let cols = ["1", "2", "3", "4"];
+let coord = [];
+
 
 const seatGrid = (props) => {
 
+    let allSeats = rows.map(row => {
+        let rowSeats = cols.map(col => <Seat>{row}{col}</Seat>);
+        return (
+            <div>
+                {rowSeats}
+            </div>
+        )
+    })
+
+
     return (
-        <div>
-            seat Grid
-            <Seat/>
+        <div className={classes.SeatGrid}>
+            <p>Screen Here</p>
+            {allSeats}
+            <p>Back Wall Here</p>
         </div>
     )
 }
