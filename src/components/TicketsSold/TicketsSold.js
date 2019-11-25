e128759eb0bacf2aaf2ea6b56cd85b78a3889868
Oscar Ko <OscarKoEmail@gmail.com>
2019-11-25 12:30:53 -0500

    Add conditional updating for ticket list

--- src/components/TicketsSold/TicketsSold.js
+++ src/components/TicketsSold/TicketsSold.js
@@ -1,43 +1,53 @@
-import React from "react";
+import React, {Component} from "react";
 import Backdrop from "../Backdrop/Backdrop";
 import TicketCard from "./TicketCard/TicketCard";
 import classes from "./TicketsSold.module.css"
 
-const ticketsSold = (props) => {
+class ticketsSold extends Component {
 
-    let allTickets = null;
-
-    if (props.selectedMovie.orders && props.selectedMovie.orders.length > 0) {
-        allTickets = props.selectedMovie.orders.map(ticket => {
-            let num = Math.random();
-            return (
-                <TicketCard 
-                    key={ticket._id + num} 
-                    ticket={ticket}
-                    deleteOrder={props.deleteOrder}
-                    editButton={props.editButton}/> 
-            )
-        })
+    componentDidUpdate() {
+        console.log("tickets sold updated");
     }
 
-    return (
-        <React.Fragment>
-            <Backdrop 
-                show={props.showTicketsSold} 
-                clicked={props.toggleTicketsSold}/>
-            <div
-                className={classes.TicketsSold}
-                style={{
-                    transform: props.showTicketsSold ? "translateY(0)" : "translateY(100vh)",
-                    opacity: props.showTicketsSold ? "1" : "0"
-                }}>
-                <p onClick={props.toggleTicketsSold}>X Cancel</p>
-                <h1>{props.selectedMovie.title}</h1>
-                <h2>Purchased Ticket Orders</h2>
-                {allTickets}
-            </div>
-        </React.Fragment>
-    )
+    shouldComponentUpdate(nextProps) {
+        return nextProps.showTicketsSold || nextProps.showTicketsSold !== this.props.showTicketsSold;
+    }
+
+    render() {
+        let allTickets = null;
+
+        if (this.props.selectedMovie.orders && this.props.selectedMovie.orders.length > 0) {
+            allTickets = this.props.selectedMovie.orders.map(ticket => {
+                let num = Math.random();
+                return (
+                    <TicketCard 
+                        key={ticket._id + num} 
+                        ticket={ticket}
+                        deleteOrder={this.props.deleteOrder}
+                        editButton={this.props.editButton}/> 
+                )
+            })
+        }
+
+        return (
+            <React.Fragment>
+                <Backdrop 
+                    show={this.props.showTicketsSold} 
+                    clicked={this.props.toggleTicketsSold}/>
+                <div
+                    className={classes.TicketsSold}
+                    style={{
+                        transform: this.props.showTicketsSold ? "translateY(0)" : "translateY(100vh)",
+                        opacity: this.props.showTicketsSold ? "1" : "0"
+                    }}>
+                    <p onClick={this.props.toggleTicketsSold}>X Cancel</p>
+                    <h1>{this.props.selectedMovie.title}</h1>
+                    <h2>Purchased Ticket Orders</h2>
+                    {allTickets}
+                </div>
+            </React.Fragment>
+        )
+    }
 }
 
 export default ticketsSold;
