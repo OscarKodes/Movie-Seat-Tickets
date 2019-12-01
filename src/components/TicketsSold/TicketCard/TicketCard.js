b9e9fb504b394d1e4c5ea72e7508350f35a3e120
Oscar Ko <OscarKoEmail@gmail.com>
2019-12-01 15:00:08 -0500

    Stylize ticket order cards

--- src/components/TicketsSold/TicketCard/TicketCard.js
+++ src/components/TicketsSold/TicketCard/TicketCard.js
@@ -5,30 +5,35 @@ const ticketCard = (props) => {
 
     return (
         <div className={classes.TicketCard}>
-            <table>
-                <tbody>
-                    <tr>
-                        <td>Date: </td><td>{props.ticket.showDate}</td>
-                    </tr>
-                    <tr>
-                        <td>Time: </td><td>{props.ticket.showTime}</td>
-                    </tr>
-                    <tr>
-                        <td>Seat: </td><td>{props.ticket.selectedSeat}</td>
-                    </tr>
-                    <tr>
-                        <td>Age: </td><td>{props.ticket.age}</td>
-                    </tr>
-                    <tr>
-                        <td>Email: </td><td>{props.ticket.email}</td>
-                    </tr>
-                    <tr>
-                        <td>Price: </td><td>${Number(props.ticket.cost).toFixed(2)}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <button onClick={() => props.editButton(props.ticket)}>Edit</button>
-            <button onClick={() => props.deleteOrder(props.ticket._id)}>Delete</button>
+            <div className={classes.FlexContainer}>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Date: </td><td>{props.ticket.showDate}</td>
+                        </tr>
+                        <tr>
+                            <td>Time: </td><td>{props.ticket.showTime}</td>
+                        </tr>
+                        <tr>
+                            <td>Seat: </td><td>{props.ticket.selectedSeat}</td>
+                        </tr>
+                        <tr>
+                            <td>Age: </td><td>{props.ticket.age}</td>
+                        </tr>
+                        <tr>
+                            <td>Email: </td><td>{props.ticket.email}</td>
+                        </tr>
+                        <tr>
+                            <td>Price: </td><td>${Number(props.ticket.cost).toFixed(2)}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div className={classes.ButtonContainer}>
+                    <div className={classes.EditBtn} onClick={() => props.editButton(props.ticket)}>Edit</div>
+                    <div className={classes.DeleteBtn} onClick={() => props.deleteOrder(props.ticket._id)}>Delete</div>
+                </div>
+            </div>
+
         </div>
     )
 }
