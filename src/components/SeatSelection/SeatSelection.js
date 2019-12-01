7adb67397350f3eb009f649d0d45d39d3211b865
Oscar Ko <OscarKoEmail@gmail.com>
2019-12-01 16:03:52 -0500

    Stylize seat selection form

--- src/components/SeatSelection/SeatSelection.js
+++ src/components/SeatSelection/SeatSelection.js
@@ -24,7 +24,7 @@ class seatSelection extends Component {
             discounts.push("Matinee");
         }
 
-        let discountNote = discounts.length > 0 ? <p>Discounts: {discounts.join(" & ")}</p> : null;
+        let discountNote = discounts.length > 0 ? <p>{discounts.join(" & ")}</p> : null;
 
         return (
             <React.Fragment>
@@ -45,36 +45,65 @@ class seatSelection extends Component {
                         allOrders={this.props.selectedMovie.orders}
                         selectedTime={this.props.selectedTime}
                         editTicketSeat={this.props.editTicketSeat}/>
-                    <p>Selected Seat: {this.props.selectedSeat}</p>
-                    <p>Movie: {this.props.selectedMovie.title}</p>
-                    <p>Time: {this.props.selectedTime}</p>
-                    <label>Email
-                        <input 
-                            type="text" 
-                            placeholder="QueenElsa@Arendelle.com"
-                            onChange={this.props.emailChangeHandler}
-                            value={this.props.email}/>
-                    </label>
-                    <label>Age
-                        <select 
-                            onChange={this.props.ageChangeHandler}
-                            value={this.props.age}>
-                            <option value="">Select Age</option>
-                            <option value="Adult">Adult 18+</option>
-                            <option value="Child">Child 0-17</option>
-                            <option value="Elder">Elder 55+</option>
-                        </select>
-                    </label>
-                    {discountNote}
-                    <label>Price
-                        <p><strong>${ticketPrice.toFixed(2)}</strong></p>
-                    </label>
-                    <button onClick={() => this.props.purchase(ticketPrice.toFixed(2))}>
+                    <table className={classes.PurchaseTable}>
+                        <tbody>
+                            <tr>
+                                <td>Seat: </td>
+                                <td>{this.props.selectedSeat}</td>
+                            </tr>
+                            <tr>
+                                <td>Movie: </td>
+                                <td>{this.props.selectedMovie.title}</td>
+                            </tr>
+                            <tr>
+                                <td>Time: </td>
+                                <td>{this.props.selectedTime}</td>
+                            </tr>
+                            <tr>
+                                <td>Email: </td>
+                                <td>
+                                    <input 
+                                    type="text" 
+                                    placeholder="QueenElsa@Arendelle.com"
+                                    onChange={this.props.emailChangeHandler}
+                                    value={this.props.email}/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Age: </td>
+                                <td>
+                                    <select 
+                                        onChange={this.props.ageChangeHandler}
+                                        value={this.props.age}>
+                                        <option value="">Select Age</option>
+                                        <option value="Adult">Adult 18+</option>
+                                        <option value="Child">Child 0-17</option>
+                                        <option value="Elder">Elder 55+</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Discounts: </td>
+                                <td>
+                                    {discountNote}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Price: </td>
+                                <td>
+                                    <strong>${ticketPrice.toFixed(2)}</strong>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div 
+                        className={classes.PurchaseBtn}
+                        onClick={() => this.props.purchase(ticketPrice.toFixed(2))}
+                        color="green">
                         {this.props.editTicketSeat ? "Update" : "Purchase"}
-                    </button>
+                    </div>
                 </div>
             </React.Fragment>
-
         )
     }
 }
