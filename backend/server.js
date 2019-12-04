2c9b6110c693750dcfcd497238814d8bc0c861af
Oscar Ko <OscarKoEmail@gmail.com>
2019-12-04 15:40:27 -0500

    Procfile starts node and npm, other files are reset back to before Heroku

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-// const port = process.env.PORT || 5000;
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // REQUIRE ROUTE FILES ==========================
 const movieRoutes = require("./routes/movies");
