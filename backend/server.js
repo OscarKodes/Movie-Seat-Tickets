a24a95ecc671fa50e1963a3bf81f1d03a7ae0a5e
Oscar Ko <OscarKoEmail@gmail.com>
2019-12-04 14:59:37 -0500

    Try 'cannot get' fix custom

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,13 @@ const port = process.env.PORT || 5000;
 
 // IF HEROKU ====================================
 if (process.env.NODE_ENV === 'production') {
-    
+
     // Express will serve up production assets
-    app.use(express.static('client/build'));
+    app.use(express.static(__dirname + '/public'));
 
     // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.get("/", (req, res) => {
+        res.render("index.html");
     });
 }
 
